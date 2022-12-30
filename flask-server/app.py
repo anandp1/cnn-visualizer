5f3db3f1c800afaf0e5fc36dc9816bfb97c39003
Anand Patel <anandpp2001@gmail.com>
2022-12-30 11:24:15 -0700

    format code

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,17 +1,18 @@
 from flask import Flask
 from flask import request
+
 from predict import predict_digit
 
 app = Flask(__name__)
 
+
 @app.route("/predict", methods=["POST"])
 def predict():
-    print("here");
     data = request.get_json(force=True)
     test = predict_digit(data)
 
-    print(test)
-    return {"prediction" : int(test)}
+    return {"prediction": int(test)}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
