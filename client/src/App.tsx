e6dab57c04b478ac98ea9be19ed4951e0bafc34e
Anand Patel <anandpp2001@gmail.com>
2022-12-30 11:21:18 -0700

    Rm commented code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,8 @@
-import { useState, useEffect } from "react";
 import Canvas from "./canvas";
 
 function App() {
-  const [data, setData] = useState({ members: [] });
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const result = await fetch("/members");
-  //     const body = await result.json();
-  //     setData(body);
-  //   };
-  //   fetchData();
-  // }, []);
-
-  // if (data.members.length === 0) {
-  //   return <p>Loading...</p>;
-  // }
-
   return (
     <div className="bg-gradient-to-b from-sky-700 to-sky-900 w-full h-screen">
-      {/* {data.members.map((member: any) => (
-        <div key={member}>
-          <h1 className="text-sm text-blue-500">{member}</h1>
-        </div>
-      ))} */}
       <Canvas />
     </div>
   );
